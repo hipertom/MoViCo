e9158a9599e232eee9ead5551144a97916e2d34d
Tom Grootjans <tom.grootjans@gmail.com>
2017-11-13 14:03:47 +0100

    sass variables and coloring

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,4 +1,4 @@
-<header class="jumbotron text-center">
+<header class="text-center">
   <div class="container">
       <img class="sitelogo" src="{{ asset('images/logo.png') }}" alt="logo">
       <h1 class="siteheading">MoViCo</h1>
