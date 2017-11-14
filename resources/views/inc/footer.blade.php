26411832387a83635526aeeaaf05714536e5a54e
Tom Grootjans <tom.grootjans@gmail.com>
2017-11-14 15:07:10 +0100

    laravel tag instead of php tag

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-md-12">
-        <p>Copyright&copy; 2015-<?php echo date("Y"); ?> MoViCo, <a href="http://tomgrootjans.nl">Tom Grootjans</a></p>
+        <p>Copyright&copy; 2015-{{date("Y")}} MoViCo, <a href="http://tomgrootjans.nl">Tom Grootjans</a></p>
       </div>
     </div>
   </div>
