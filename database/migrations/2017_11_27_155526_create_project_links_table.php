8ab70ca704685e1dccca1bf56f5844a94c6233f6
Tom Grootjans <tom.grootjans@gmail.com>
2017-11-27 17:04:38 +0100

    crating models and DB for projects

--- database/migrations/2017_11_27_155526_create_project_links_table.php
+++ database/migrations/2017_11_27_155526_create_project_links_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMessagesTable extends Migration
+class CreateProjectLinksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,13 @@ class CreateMessagesTable extends Migration
      */
     public function up()
     {
-        Schema::create('messages', function (Blueprint $table) {
+        Schema::create('project_links', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email');
-            $table->mediumText('message');
+
+            $table->integer('project_id');
+            $table->string('type');
+            $table->string('url');
+
             $table->timestamps();
         });
     }
@@ -29,6 +31,6 @@ class CreateMessagesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('messages');
+        Schema::dropIfExists('project_links');
     }
 }
