3c49483daa020f411bcc0a39160d4a06d4b3a6a8
Tom Grootjans <tom.grootjans@gmail.com>
2017-11-28 16:43:10 +0100

    added relations in Project model

--- app/Project.php
+++ app/Project.php
@@ -6,8 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    public function links()
+    public function languages()  // fetch all languages associated with the project
     {
-      return $this->hasMany(Comment::class);
+      return $this->hasMany(Language::class);
     }
+
+    public function frameworks() // fetch all frameworks associated with the project
+    {
+      return $this->hasMany(Framework::class);
+    }
+
+    public function cms() // fetch all cms's associated with the project
+    {
+      return $this->hasMany(Cms::class);
+    }
+
+    public function links() // fetch all links associated with the project
+    {
+      return $this->hasMany(Link::class);
+    }
+
 }
