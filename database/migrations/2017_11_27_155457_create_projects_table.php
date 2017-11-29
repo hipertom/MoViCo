fdda83ceabbddaa283211f7a310e0bf6dc3a3de4
Tom Grootjans <tom.grootjans@gmail.com>
2017-11-29 11:20:18 +0100

    added unique index to some db columns

--- database/migrations/2017_11_27_155457_create_projects_table.php
+++ database/migrations/2017_11_27_155457_create_projects_table.php
@@ -16,7 +16,7 @@ class CreateProjectsTable extends Migration
         Schema::create('projects', function (Blueprint $table) {
           $table->increments('id');
 
-          $table->string('name');
+          $table->string('name')->unique();
           $table->string('image');
           $table->text('description');
           $table->integer('order');
--- database/migrations/2017_11_27_174106_create_link_types_table.php
+++ database/migrations/2017_11_27_174106_create_link_types_table.php
@@ -16,7 +16,7 @@ class CreateLinkTypesTable extends Migration
         Schema::create('link_types', function (Blueprint $table) {
             $table->increments('id');
 
-            $table->string('name');
+            $table->string('name')->unique();
 
             $table->timestamps();
         });
