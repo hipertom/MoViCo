3250ab4f4b27e2f970394f4a1a909f4e99c0fbdf
Tom Grootjans <tom.grootjans@gmail.com>
2017-12-03 19:58:21 +0100

    added row

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,50 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="row">
+    <div class="col-md-2 col-md-offset-1">
+        <a href="#">
+            <div class="box">
+                <span class="icon"><i class="fa fa-pie-chart" aria-hidden="true"></i></span>
+                <span class="num">420</span>
+            </div>
+        </a>
+    </div>
 
-<div class="col-md-2 col-md-offset-1">
+    <div class="col-md-2">
     <a href="#">
-        <div class="box">
-            <span class="icon"><i class="fa fa-pie-chart" aria-hidden="true"></i></span>
-            <span class="num">420</span>
-        </div>
-    </a>
-</div>
+            <div class="box">
+                <span class="icon"><i class="fa fa-pie-chart" aria-hidden="true"></i></span>
+                <span class="num">420</span>
+            </div>
+        </a>
+    </div>
 
-<div class="col-md-2">
-   <a href="#">
-        <div class="box">
-            <span class="icon"><i class="fa fa-pie-chart" aria-hidden="true"></i></span>
-            <span class="num">420</span>
-        </div>
-    </a>
-</div>
+    <div class="col-md-2">
+        <a href="#">
+            <div class="box">
+                <span class="icon"><i class="fa fa-pie-chart" aria-hidden="true"></i></span>
+                <span class="num">420</span>
+            </div>
+        </a>
+    </div>
 
-<div class="col-md-2">
-    <a href="#">
-        <div class="box">
-            <span class="icon"><i class="fa fa-pie-chart" aria-hidden="true"></i></span>
-            <span class="num">420</span>
-        </div>
-    </a>
-</div>
+    <div class="col-md-2">
+        <a href="#">
+            <div class="box">
+                <span class="icon"><i class="fa fa-pie-chart" aria-hidden="true"></i></span>
+                <span class="num">420</span>
+            </div>
+        </a>
+    </div>
 
-<div class="col-md-2">
-    <a href="#">
-        <div class="box">
-            <span class="icon"><i class="fa fa-pie-chart" aria-hidden="true"></i></span>
-            <span class="num">420</span>
-        </div>
-    </a>
-</div>
-
-<div class="col-md-2">
-    <a href="#">
-        <div class="box">
-            <span class="icon"><i class="fa fa-pie-chart" aria-hidden="true"></i></span>
-            <span class="num">420</span>
-        </div>
-    </a>
+    <div class="col-md-2">
+        <a href="#">
+            <div class="box">
+                <span class="icon"><i class="fa fa-pie-chart" aria-hidden="true"></i></span>
+                <span class="num">420</span>
+            </div>
+        </a>
+    </div>
 </div>
 
 
