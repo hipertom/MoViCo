7ea5d17fcac9b2f82be12d40e36853d02ddf723d
Tom Grootjans <tom.grootjans@gmail.com>
2017-12-03 19:25:22 +0100

    added box and styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,50 @@
 
 @section('content')
 
-<h1>Home</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<div class="col-md-2 col-md-offset-1">
+    <a href="#">
+        <div class="box">
+            <span class="icon"><i class="fa fa-pie-chart" aria-hidden="true"></i></span>
+            <span class="num">420</span>
+        </div>
+    </a>
+</div>
+
+<div class="col-md-2">
+   <a href="#">
+        <div class="box">
+            <span class="icon"><i class="fa fa-pie-chart" aria-hidden="true"></i></span>
+            <span class="num">420</span>
+        </div>
+    </a>
+</div>
+
+<div class="col-md-2">
+    <a href="#">
+        <div class="box">
+            <span class="icon"><i class="fa fa-pie-chart" aria-hidden="true"></i></span>
+            <span class="num">420</span>
+        </div>
+    </a>
+</div>
+
+<div class="col-md-2">
+    <a href="#">
+        <div class="box">
+            <span class="icon"><i class="fa fa-pie-chart" aria-hidden="true"></i></span>
+            <span class="num">420</span>
+        </div>
+    </a>
+</div>
+
+<div class="col-md-2">
+    <a href="#">
+        <div class="box">
+            <span class="icon"><i class="fa fa-pie-chart" aria-hidden="true"></i></span>
+            <span class="num">420</span>
+        </div>
+    </a>
+</div>
+
 
 @endsection
