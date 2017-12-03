1040cb2fa9923ec0a77e93828dc3a4baca2761ba
Tom Grootjans <tom.grootjans@gmail.com>
2017-12-03 21:24:20 +0100

    display var with analytics data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,9 @@
 <div class="row">
     <div class="col-md-2 col-md-offset-1">
         <a href="#">
-            <div class="box">
+            <div class="box" data-toggle="tooltip" data-placement="bottom" title="Number of visitors last week">
                 <span class="icon"><i class="fa fa-pie-chart" aria-hidden="true"></i></span>
-                <span class="num">420</span>
+                <span class="num" id="ganumber">{{$file}}</span>
             </div>
         </a>
     </div>
