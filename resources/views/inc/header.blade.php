a8157ca24bfe3e213acd0395bb2455ab829ad1d2
Tom Grootjans <tom.grootjans@gmail.com>
2017-12-03 15:51:52 +0100

    added tooltip to hamburger menu toggle

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,5 +1,5 @@
 <div class="sidebar-toggle-box">
-    <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+    <div class="fa fa-bars" data-toggle="tooltip" data-placement="right" title="Toggle navigation"></div>
 </div>
 
 <a href="index.html" class="logo"><b>MoViCo</b></a>
