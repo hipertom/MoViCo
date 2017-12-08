1a5d61f14bc06c481b5db1a5c9c87aa34ab21a7a
Tom Grootjans <tom.grootjans@gmail.com>
2017-12-08 11:14:37 +0100

    initial content for right sidebar

--- resources/views/inc/rightsidebar.blade.php
+++ resources/views/inc/rightsidebar.blade.php
@@ -1,1 +1,6 @@
-rightsidebar
+<section>
+    <h3>Initial content</h3>
+    <p>
+    Exercitation proident qui ullamco commodo occaecat. Nulla tempor incididunt commodo dolor esse culpa. Voluptate aute velit eiusmod et et enim dolor aliquip exercitation magna. Excepteur occaecat labore commodo eiusmod do aliquip ullamco.
+    </p>
+</section>
