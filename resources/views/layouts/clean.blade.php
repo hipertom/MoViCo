38964316f271d294c2348453fa06486d2512ee3f
Tom Grootjans <tom.grootjans@gmail.com>
2017-12-12 16:47:27 +0100

    added clean layout (no menu/sidebar and stuff)

---
+++ resources/views/layouts/clean.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>MoViCo</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link href="/css/app.css" rel="stylesheet">
+    </head>
+    <body>
+
+        @include('inc.errorMessages')
+
+          <!-- main content -->
+          <section class="container cleanlayout">
+              <div class="row">
+                <!-- content -->
+                <div class="col-md-12">
+                  @yield('content')
+                </div>
+              </div>
+            </div>
+          </section>
+
+        </div>
+
+        @include('inc.footer')
+    </body>
+</html>
