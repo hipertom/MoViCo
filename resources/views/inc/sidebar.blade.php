f1ac7c9348815db42c4dc2ee5742e5347d4f72df
Tom Grootjans <tom.grootjans@gmail.com>
2017-12-12 23:48:45 +0100

    moved logout from header to sidebar (needs styling)

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -1,43 +1,51 @@
 <ul class="sidebar-menu">
+    
+    @Auth
+    <p class="centered"><a href="profile.html"><img src="{{asset('images/avatars/man-2.png')}}" class="img-circle" width="60"></a></p>
+    <h5 class="centered">{{ Auth::user()->name }}</h5>
+    <a class="centered" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+    @else
+    <p>error: must be logged in</p>
+    @endAuth
 
-  <p class="centered"><a href="profile.html"><img src="{{asset('images/avatars/man-2.png')}}" class="img-circle" width="60"></a></p>
-  <h5 class="centered">Account Name</h5>
-
-  <li class="">
-      <a class="{{Request::is('/') || Request::is('home') ? "active" : ""}}" href="/home">
-          <i class="fa fa-dashboard"></i>
-          <span>Dashboard</span>
-      </a>
-  </li>
-
-  <li class="">
-      <a class="{{Request::is('about') ? "active" : ""}}" href="/about">
-          <i class="fa fa-user-circle-o"></i>
-          <span>About</span>
-      </a>
-  </li>
-
-  <li class="">
-      <a class="{{Request::is('projects') ? "active" : ""}}" href="/projects">
-          <i class="fa fa-id-card"></i>
-          <span>Projects</span>
-      </a>
-  </li>
-
-  <li class="">
-      <a class="{{Request::is('contact') ? "active" : ""}}" href="/contact">
-          <i class="fa fa-address-book"></i>
-          <span>Contact</span>
-      </a>
-  </li>
-
-  <li class="">
-      <a class="{{Request::is('projectsJSON') ? "active" : ""}}" href="/projectsJSON">
-          <i class="fa fa-file-text"></i>
-          <span>Project JSON</span>
-      </a>
-  </li>
+    <!-- hidden logout form -->
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        {{ csrf_field() }}
+    </form>
 
 
+    <li class="">
+        <a class="{{Request::is('/') || Request::is('home') ? "active" : ""}}" href="/home">
+            <i class="fa fa-dashboard"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
 
-  </ul>
+    <li class="">
+        <a class="{{Request::is('about') ? "active" : ""}}" href="/about">
+            <i class="fa fa-user-circle-o"></i>
+            <span>About</span>
+        </a>
+    </li>
+
+    <li class="">
+        <a class="{{Request::is('projects') ? "active" : ""}}" href="/projects">
+            <i class="fa fa-id-card"></i>
+            <span>Projects</span>
+        </a>
+    </li>
+
+    <li class="">
+        <a class="{{Request::is('contact') ? "active" : ""}}" href="/contact">
+            <i class="fa fa-address-book"></i>
+            <span>Contact</span>
+        </a>
+    </li>
+
+    <li class="">
+        <a class="{{Request::is('projectsJSON') ? "active" : ""}}" href="/projectsJSON">
+            <i class="fa fa-file-text"></i>
+            <span>Project JSON</span>
+        </a>
+    </li>
+</ul>
