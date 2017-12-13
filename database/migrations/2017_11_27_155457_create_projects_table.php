fdf173be70a29fd379a83de265d72560370c675c
Tom Grootjans <tom.grootjans@gmail.com>
2017-12-13 23:19:57 +0100

    added nullable index to order field on projects table

--- database/migrations/2017_11_27_155457_create_projects_table.php
+++ database/migrations/2017_11_27_155457_create_projects_table.php
@@ -16,7 +16,7 @@ class CreateProjectsTable extends Migration
         Schema::create('projects', function (Blueprint $table) {
           $table->increments('id');
 
-          $table->integer('order');
+          $table->integer('order')->nullable();
           $table->string('name')->unique();
           $table->text('description');
           $table->string('image')->nullable();
