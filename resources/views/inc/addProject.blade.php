a2378e3acf1444801b58c50b25a08d24a9098c12
Tom Grootjans <tom.grootjans@gmail.com>
2017-12-13 23:11:31 +0100

    add project form changes

--- resources/views/inc/addProject.blade.php
+++ resources/views/inc/addProject.blade.php
@@ -1,35 +1,38 @@
 <section class="addProjects">
-<h3>Add project</h3>
-    {!! Form::open(['url' => 'contact/submit']) !!}
+    <h3>Add project</h3>
+    {!! Form::open(['url' => 'projects/add', 'files' => 'true']) !!}
+        
+        <!-- name -->
         <div class="form-group @if($errors->has('name')) has-warning @endif">
         {{Form::label('name', 'Name')}}
         {{Form::text('name', null ,['class' => 'form-control', 'placeholder' => 'John Doe'])}}
-
         @foreach($errors->get('name') as $error)
-            <span id="helpBlock2" class="help-block">{{$error}}</span>
+            <span id="name" class="help-block">{{$error}}</span>
         @endforeach
         </div>
-        <div class="form-group @if($errors->has('email')) has-warning @endif">
-        {{Form::label('email', 'E-Mail')}}
-        {{Form::email('email', null, ['class' => 'form-control','placeholder' => 'you@domain.com'])}}
-        @foreach($errors->get('email') as $error)
-            <span id="helpBlock2" class="help-block">{{$error}}</span>
+        
+        <!-- Image -->
+        <div class="form-group @if($errors->has('image')) has-warning @endif">
+        {{Form::label('image', 'Image')}}
+        {{Form::file('image')}}
+        @foreach($errors->get('image') as $error)
+            <span id="image" class="help-block">{{$error}}</span>
         @endforeach
         </div>
-        <div class="form-group @if($errors->has('phone')) has-warning @endif">
-        {{Form::label('phone', 'Phone Number')}}
-        {{Form::tel('phone', null, ['class' => 'form-control','placeholder' => '0612345678'])}}
-        @foreach($errors->get('phone') as $error)
+        
+        
+        
+        <!-- Description -->
+        <div class="form-group @if($errors->has('description')) has-warning @endif">
+        {{Form::label('description', 'Description')}}
+        @foreach($errors->get('description') as $error)
             <span id="helpBlock2" class="help-block">{{$error}}</span>
         @endforeach
+        {{Form::textarea('description', null, ['class' => 'form-control', 'rows' => '3','placeholder' => 'John Doe'])}}
         </div>
-        <div class="form-group @if($errors->has('message')) has-warning @endif">
-        {{Form::label('message', 'Message')}}
-        @foreach($errors->get('message') as $error)
-            <span id="helpBlock2" class="help-block">{{$error}}</span>
-        @endforeach
-        {{Form::textarea('message', null, ['class' => 'form-control', 'rows' => '3','placeholder' => 'John Doe'])}}
-        </div>
+
+        <!-- submit -->
         {{Form::submit('Submit', ['class' => 'btn btn-default'])}}
+
     {!! Form::close() !!}
 </section>
