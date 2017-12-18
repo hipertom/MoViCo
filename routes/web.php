d776b3d3549cd3aaa6dc4f5401b35f0927412345
Tom Grootjans <tom.grootjans@gmail.com>
2017-12-18 14:46:18 +0100

    tweaking routes

--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,22 @@ Auth::routes();
 
 // Basic pages
 Route::get('/', 'ViewController@getHome');
-Route::get('/home', 'ViewController@getHome');
-Route::get('/about', 'ViewController@getAbout');
-Route::get('/contact', 'ViewController@getContact');
+Route::get('home', 'ViewController@getHome');
+Route::get('about', 'ViewController@getAbout');
+Route::get('contact', 'ViewController@getContact');
 
 
 // Projects
-Route::get('/projects', 'ProjectsController@getProjects');
-Route::post('/projects/add', 'ProjectsController@addProject'); // add projects
-Route::get('/projectsJSON', 'ProjectsController@getProjectsJSON'); // json output
+Route::get('projects', 'ProjectsController@getProjects');
+Route::post('projects/add', 'ProjectsController@addProject'); // add projects
+Route::get('projectsJSON', 'ProjectsController@getProjectsJSON'); // json output
+
+Route::get('project/delete/{id}', 'ProjectsController@deleteProject')->where('id', '[0-9]+');
 
 // Messages (todo: delete)
-Route::get('/messages', 'messagesController@getMessages');
+Route::get('messages', 'messagesController@getMessages');
 
 // Account stuff
-Route::get('/logout', 'ViewController@getLogout');
+Route::get('logout', 'ViewController@getLogout');
 
 
