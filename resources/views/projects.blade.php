8c2a3583363b88febcbc1cb8ffc3572a8dc93476
Tom Grootjans <tom.grootjans@gmail.com>
2017-12-18 10:45:37 +0100

    displaying all links

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -22,7 +22,14 @@
               <td>{{ !is_null($project->order)? $project->order : "no order"  }}</td>
               <td>{{$project->name}} image: '{{$project->image}}'</td>
               <td>{{ str_limit($project->description, $limit = 100, $end = '...') }}</td>
-              <td>links</td>
+              <td>
+              @foreach ($project->links as $link)
+                <a href="{{$link->url}}" class="btn btn-success btn-xs" title="{{$link->type->name}}" target="_blank">
+                  <i class="fa fa-paper-plane"></i>
+                </a>
+                
+              @endforeach
+              </td>
               <td><span class="label label-info label-mini">{{ !is_null($project->status)? $project->status->name : "no status"}}</span></td>
               <td>
                 <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
