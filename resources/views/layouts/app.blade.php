6749692607aceaca364983f22e0e49354846051c
Tom Grootjans <tom.grootjans@gmail.com>
2018-03-03 15:05:46 +0100

    use favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,9 +2,13 @@
 <html lang="en">
     <head>
         <title>MoViCo</title>
+        
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        
+        <link rel="icon" type="image/png" href="/images/favicon.png">
+
         <link href="/css/app.css" rel="stylesheet">
     </head>
     <body>
