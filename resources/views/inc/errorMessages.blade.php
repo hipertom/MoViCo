aeca0df253968fcc9226943892e1a4b2a124026b
Tom Grootjans <tom.grootjans@gmail.com>
2018-04-02 17:52:19 +0200

    Show error messages in rightsidebar

--- resources/views/inc/errorMessages.blade.php
+++ resources/views/inc/errorMessages.blade.php
@@ -1,6 +1,16 @@
-@if(session('messageSendSucess'))
-<div class="alert alert-success alert-dismissible" role="alert">
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  {{session('messageSendSucess')}}
-</div>
+@if(session('errorMessage'))
+    @section('rightsidebar')
+        <section>
+            <div class="alert alert-success alert-dismissible" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                {{session('errorMessage')}}
+            </div>
+        </section>
+            
+        @parent
+    @endsection
 @endif
+
+
+
+    
